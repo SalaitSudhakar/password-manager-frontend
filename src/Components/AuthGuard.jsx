f9ef8c0300277e993a4127c3be13d91b97f5784b
SalaitSudhakar <salaitsudhakar.s@gmail.com>
2025-04-14 10:35:11 +0530

    Add emailverified true to authGuard

--- src/Components/AuthGuard.jsx
+++ src/Components/AuthGuard.jsx
@@ -3,9 +3,10 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const AuthGuard = () => {
-  const { isAuthenticated } = useSelector((state) => state.user);
+  const { isAuthenticated, userDetails } = useSelector((state) => state.user);
+  const emailVerified = userDetails?.user?.emailVerified || false;
 
-  return isAuthenticated ? <Navigate to="/" replace /> : <Outlet />;
+  return isAuthenticated && emailVerified ? <Navigate to="/" replace /> : <Outlet />;
 };
 
 export default AuthGuard;
